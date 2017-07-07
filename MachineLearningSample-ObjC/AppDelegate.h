4c3529fa4635ce231a069803de896db125d2226b
Atif Imran <mann@atif-imrans-mac-mini.local>
2017-07-07 10:42:53 +0530

    Initial Commit

---
+++ MachineLearningSample-ObjC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MachineLearningSample-ObjC
+//
+//  Created by Atif Imran on 7/7/17.
+//  Copyright © 2017 Atif Imran. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MachineLearningSample-ObjC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MachineLearningSample-ObjC
+//
+//  Created by Atif Imran on 7/7/17.
+//  Copyright © 2017 Atif Imran. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
